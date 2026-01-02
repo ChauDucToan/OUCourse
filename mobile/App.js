6ef7830e0bcfe4f4526249aa448b1c133c913b8f
khaquachtrong <khaquachtrong74@gmail.com>
2026-01-02 14:43:16 +0700

    feat(mobile): create screen Home and adding to App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,93 @@
-import Card from "./components/Card";
-import "./global.css";
-import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./screens/Home/Home";
+import { useReducer } from "react";
+import MyReducers from "./utils/reducers/MyReducers";
+import { MyUserContext } from "./utils/contexts/MyContext";
+import { Card, Icon } from "react-native-paper";
+import { useContext } from "react";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+import Register from "./screens/User/Register";
+import Login from "./screens/User/Login";
+import User from "./screens/User/User";
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name={"Course"}
+        component={Home}
+        options={{ title: "Trang chủ" }}
+      />
+      <Stack.Screen
+        name="MyLearning"
+        component={Card}
+        options={{ title: "card ne" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const TabNavigator = () => {
+  const [user] = useContext(MyUserContext);
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name={"Home"}
+        component={StackNavigator}
+        options={{
+          title: "Trang chủ",
+          tabBarIcon: () => <Icon color="blue" source="home" size={30} />,
+        }}
+      />
+      {user === null ? (
+        <>
+          <Tab.Screen
+            name="Register"
+            component={Register}
+            options={{
+              title: "Đăng ký",
+              tabBarIcon: () => (
+                <Icon color="blue" source="account" size={30} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Login"
+            component={Login}
+            options={{
+              title: "Đăng nhập",
+              tabBarIcon: () => <Icon color="blue" source="login" size={30} />,
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <Tab.Screen
+            name="Profile"
+            component={User}
+            options={{
+              title: "Profile",
+              tabBarIcon: () => (
+                <Icon color="blue" source="account" size={30} />
+              ),
+            }}
+          />
+        </>
+      )}
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
+  const [user, dispatch] = useReducer(MyReducers, null);
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-      <Card text="Khóa luyện tinh thần" price="500.000.000VND" author="DTK" />
-      <Card
-        text="Khóa luyện nội công"
-        price="236.000.000VND"
-        author="Oslamelon"
-      />
-    </View>
+    <MyUserContext.Provider value={[user, dispatch]}>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </MyUserContext.Provider>
   );
 }
