7e8f88be71e491a4a660a57d18ca608dfe110bbc
khaquachtrong <khaquachtrong74@gmail.com>
2026-01-02 17:16:53 +0700

    feat(frontend): Update App

--- mobile/App.js
+++ mobile/App.js
@@ -1,3 +1,4 @@
+import "./global.css";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -7,13 +8,17 @@ import MyReducers from "./utils/reducers/MyReducers";
 import { MyUserContext } from "./utils/contexts/MyContext";
 import { Card, Icon } from "react-native-paper";
 import { useContext } from "react";
+import colors from "tailwindcss/colors";
+
+import Auth from "./screens/User/Auth";
+import User from "./screens/User/User";
+import Appearance from "./screens/Setting/Appearance";
+import Setting from "./screens/Setting/Setting";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
-import Register from "./screens/User/Register";
-import Login from "./screens/User/Login";
-import User from "./screens/User/User";
-const StackNavigator = () => {
+
+const StackHomeNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen
@@ -29,6 +34,22 @@ const StackNavigator = () => {
     </Stack.Navigator>
   );
 };
+const StackSettingNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Setting"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen
+        name="Setting"
+        component={Setting}
+        options={{ title: "Cài đặt" }}
+      />
+      <Stack.Screen name="Auth" component={Auth} />
+      <Stack.Screen name="Apperance" component={Appearance} />
+    </Stack.Navigator>
+  );
+};
 
 const TabNavigator = () => {
   const [user] = useContext(MyUserContext);
@@ -36,46 +57,37 @@ const TabNavigator = () => {
     <Tab.Navigator>
       <Tab.Screen
         name={"Home"}
-        component={StackNavigator}
+        component={StackHomeNavigator}
         options={{
           title: "Trang chủ",
-          tabBarIcon: () => <Icon color="blue" source="home" size={30} />,
+          tabBarIcon: () => (
+            <Icon color={colors.slate[500]} source="home" size={30} />
+          ),
+          tabBarActiveTintColor: colors.gray[500],
         }}
       />
-      {user === null ? (
-        <>
-          <Tab.Screen
-            name="Register"
-            component={Register}
-            options={{
-              title: "Đăng ký",
-              tabBarIcon: () => (
-                <Icon color="blue" source="account" size={30} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Login"
-            component={Login}
-            options={{
-              title: "Đăng nhập",
-              tabBarIcon: () => <Icon color="blue" source="login" size={30} />,
-            }}
-          />
-        </>
-      ) : (
-        <>
-          <Tab.Screen
-            name="Profile"
-            component={User}
-            options={{
-              title: "Profile",
-              tabBarIcon: () => (
-                <Icon color="blue" source="account" size={30} />
-              ),
-            }}
-          />
-        </>
+      <Tab.Screen
+        name={"Setting"}
+        component={StackSettingNavigator}
+        options={{
+          title: "Cài đặt",
+          tabBarIcon: () => (
+            <Icon color={colors.slate[500]} source="cog-outline" size={30} />
+          ),
+          tabBarActiveTintColor: colors.gray[500],
+        }}
+      />
+      {user && (
+        <Tab.Screen
+          name="Profile"
+          component={User}
+          options={{
+            title: "Profile",
+            tabBarIcon: () => (
+              <Icon color={colors.slate[500]} source="account" size={30} />
+            ),
+          }}
+        />
       )}
     </Tab.Navigator>
   );
