08a45d3570d67b106c514a2c85ae671a91571efb
oslamelon <91736051+ChauDucToan@users.noreply.github.com>
2026-01-03 17:34:02 +0700

    feat(oucourse): update course serializer and viewset for better view in swagger

--- OUCourse/api/courses/views.py
+++ OUCourse/api/courses/views.py
@@ -18,11 +18,14 @@ class TagView(viewsets.ViewSet, generics.ListAPIView):
     serializer_class = serializers.TagSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-class CourseView(viewsets.ModelViewSet, generics.ListAPIView):
+class CourseView(viewsets.ModelViewSet):
     queryset = serializers.Course.objects.filter(active=True)
     serializer_class = serializers.CourseSerializer
     
     def get_permissions(self):
+        if self.action == 'lessons' and self.request.method.__eq__('POST'):
+            return [IsNotStudent()]
+        
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
             return [IsNotStudent()]
         
@@ -40,20 +43,19 @@ class CourseView(viewsets.ModelViewSet, generics.ListAPIView):
             query = query.filter(category_id=cate_id)
 
         return query
-
-    @action(methods=['get'], url_path='lessons', detail=True)
-    def get_lessons(self, request, pk):
-        lessons = self.get_object().lesson_set.filter(active=True)
-        return Response(serializers.LessonSerializer(lessons, many=True).data, status=status.HTTP_200_OK)
-    
-    @action(methods=['post'], permission_classes=[IsNotStudent], url_path='lessons', detail=True)
-    def create_lesson(self, request):
-        course = self.get_object()
-        serializer = serializers.LessonSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(course=course)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    @action(methods=['post', 'get'], url_path='lessons', detail=True)
+    def lessons(self, request, pk=None):
+        if request.method.__eq__('GET'):
+            lessons = self.get_object().lesson_set.filter(active=True).order_by('order')
+            return Response(serializers.LessonSerializer(lessons, many=True).data, status=status.HTTP_200_OK)
+        elif request.method.__eq__('POST'):
+            course = self.get_object()
+            serializer = serializers.LessonSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(course=course)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     @action(methods=['post'], permission_classes=[permissions.IsAuthenticated], url_path='enroll', detail=True)
     def enroll(self, request):
