11ca93e15f397175247392dbb309f512c9b412b4
khaquachtrong <khaquachtrong74@gmail.com>
2026-01-05 05:21:37 +0700

    feat(frontend): adding Stack.Screen account detailed screen

--- mobile/navigation/AccountStack.js
+++ mobile/navigation/AccountStack.js
@@ -3,6 +3,7 @@ import Login from "../screens/User/Login";
 import Register from "../screens/User/Register";
 import Appearance from "../screens/Setting/Appearance";
 import AccountScreen from "../screens/Account/Account";
+import AccountDetailedScreen from "../screens/Account/AccountDetailed";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,10 @@ const AccountStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="AccountScreen" component={AccountScreen} />
+      <Stack.Screen
+        name="AccountDetailedScreen"
+        component={AccountDetailedScreen}
+      />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="Appearance" component={Appearance} />
