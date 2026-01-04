e000674423c7d6eb0329acf966e08c8365136389
khaquachtrong <khaquachtrong74@gmail.com>
2026-01-04 17:16:39 +0700

    feat(frontend): modify token utils with expo-secure-store

--- mobile/utils/tokenUtils.js
+++ mobile/utils/tokenUtils.js
@@ -1,12 +1,37 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { deleteItemAsync, getItemAsync, setItemAsync } from "expo-secure-store";
+
+const TOKEN_KEY = "TOKEN";
 
 export const saveTokens = async (accessToken, refreshToken) => {
-  await AsyncStorage.setItem("access_token", accessToken);
-  await AsyncStorage.setItem("refresh_token", refreshToken);
+  try {
+    const keys = JSON.stringify({
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    });
+    await setItemAsync(TOKEN_KEY, keys);
+    console.log("save token success!");
+  } catch (error) {
+    console.error("save token failed: ", error.message);
+  }
 };
 
 export const getTokens = async () => {
-  const accessToken = await AsyncStorage.getItem("access_token");
-  const refreshToken = await AsyncStorage.getItem("refresh_token");
-  return { accessToken, refreshToken };
+  try {
+    const jsonData = await getItemAsync(TOKEN_KEY);
+    console.log("get token success!");
+
+    return jsonData ? JSON.parse(jsonData) : null;
+  } catch (error) {
+    console.error("get token failed: ", error.message);
+    return null;
+  }
+};
+
+export const removeTokens = async () => {
+  try {
+    await deleteItemAsync(TOKEN_KEY);
+    console.log("remove token success!");
+  } catch (error) {
+    console.error("Remove token failed: ", error.message);
+  }
 };
