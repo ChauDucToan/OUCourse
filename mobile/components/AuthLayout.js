669a1575275d6b21bef2b7d4c9a13bc6d22e5a5b
khaquachtrong <khaquachtrong74@gmail.com>
2026-01-05 08:05:39 +0700

    feat(frontend): modify component Auth and Course View

--- mobile/components/AuthLayout.js
+++ mobile/components/AuthLayout.js
@@ -1,12 +1,18 @@
 import { View, ScrollView } from "react-native";
 import TextCustom from "./TextCustom";
+import HeaderBack from "./HeaderBack";
 
 const AuthLayout = ({ title, children }) => {
   return (
     <View className="flex-1 bg-white p-4 justify-center">
-      <View className="mb-6 items-center">
-        <TextCustom.TextFocus text={title} />
+      <View className="flex-row items-center  mb-6">
+        <HeaderBack />
+
+        <View className="ml-10 items-center">
+          <TextCustom.TextFocus text={title} />
+        </View>
       </View>
+
       {children}
     </View>
   );
--- mobile/components/CourseView.js
+++ mobile/components/CourseView.js
@@ -2,23 +2,35 @@ import { Dimensions } from "react-native";
 import { Text } from "react-native";
 import { ImageBackground } from "react-native";
 import { TouchableOpacity, View } from "react-native";
-import { RenderHTML } from "react-native-render-html";
 const CourseView = ({ navigation, item }) => {
   const { width } = Dimensions.get("window");
-
+  const formatCurrency = (amount) => {
+    return new Intl.NumberFormat("vi-VN").format(amount) + " đ";
+  };
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("Search", {
+          screen: "CourseDetailedScreen",
+          params: { id: item.id },
+        })
+      }
+    >
       <ImageBackground
         source={{ uri: item?.image }}
-        className="w-full h-40 rounded-xl mt-4"
+        className="w-full h-40 mt-5"
         style={{ width: width }}
       >
-        <View className="flex-1 rounded-xl bg-black/40 justify-center px-4">
+        <View className="flex-1 bg-black/40 rounded-none justify-center px-4">
           <Text className="text-white text-xl font-bold">
             {item?.instructor}
           </Text>
           <Text className="text-white text-base">{item?.subject}</Text>
-
+          <View>
+            <Text className="text-white text-xl  ">
+              {formatCurrency(item.price)}
+            </Text>
+          </View>
           {/* <RenderHTML source={item?.description}></RenderHTML>*/}
         </View>
       </ImageBackground>
